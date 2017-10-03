352afe4969eb537916ffd2907449d3677b49e4a5
SYLVAINCOUSDC6F\Sylvain <sylvain.cousquer@gmail.com>
2017-10-03 18:06:34 +0200

    Test refactored

---
+++ src/TDD_Kata/Bowling/Bowling/BowlingGame.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling
+{
+    public class BowlingGame
+    {
+        public int Score { get; set; }
+        private int _lastRoll = 0;
+
+        public void Roll(int pin)
+        {
+            if (_lastRoll + pin == 10)
+            {
+                Score += pin + 5;
+            }
+            else
+            {
+                Score += pin;
+            }
+            _lastRoll = pin;
+            
+        }
+    }
+}
---
+++ src/TDD_Kata/Bowling/BowlingTest/BowlingGameTest.cs
@@ -0,0 +1,57 @@
+﻿using Bowling;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingTest
+{
+    [TestFixture]
+    public class BowlingGameTest
+    {
+        private BowlingGame aGame;
+        [SetUp]
+        public void Setup()
+        {
+            aGame = new BowlingGame();
+        }
+        [Test]
+        public void BowlingGameShouldScore0WhenNoPinHaveBeenKnocked()
+        {
+
+            RollWithSameNumberOfKnockedPin(20, 0);
+            Assert.That(aGame.Score, Is.EqualTo(0));
+
+        }
+
+        [Test]
+        public void BowlingGameShouldScore20When1PinHaveBeenKnockedAtEachRoll()
+        {
+            RollWithSameNumberOfKnockedPin(20,1);
+            Assert.That(aGame.Score, Is.EqualTo(20));
+
+        }
+
+        private void RollWithSameNumberOfKnockedPin(int rollNumber, int knockedPin)
+        {
+            for (int i = 0; i < rollNumber; i++)
+            {
+                aGame.Roll(knockedPin);
+            }
+        }
+
+        [Test]
+        public void BowlingGameShouldAdd5BonusPointOnaSpare()
+        {
+           
+            aGame.Roll(9);
+            aGame.Roll(1);
+
+            Assert.That(aGame.Score, Is.EqualTo(15));
+
+        }
+
+    }
+}
